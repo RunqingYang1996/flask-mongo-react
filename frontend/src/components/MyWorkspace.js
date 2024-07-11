16f16672243d92e3a1e9cb654ef5d5d1a3e43391
runqingyang <user@10-21-162-171.dynapool.wireless.nyu.edu>
2024-07-11 09:02:46 -0400

    flipsuccess

--- frontend/src/components/MyWorkspace.js
+++ frontend/src/components/MyWorkspace.js
@@ -1,31 +1,65 @@
 import React, { useState, useEffect } from 'react';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemText from '@mui/material/ListItemText';
-import Typography from '@mui/material/Typography';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  IconButton,
+  Typography
+} from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 export default function MyWorkspace() {
   const [images, setImages] = useState([]);
 
   useEffect(() => {
-    fetch('http://127.0.0.1:5000/api/images')  // 确保URL指向Flask服务器
+    fetch('http://127.0.0.1:5000/api/images')  // Ensure this URL points to your Flask server
       .then(response => response.json())
       .then(data => setImages(data))
       .catch(error => console.error('Error fetching images:', error));
   }, []);
+
+  const handleDelete = (index) => {
+    const newData = images.filter((_, idx) => idx !== index);
+    setImages(newData);
+  };
 
   return (
     <div>
       <Typography variant="h4" component="div" sx={{ flexGrow: 1 }}>
         My Workspace
       </Typography>
-      <List>
-        {images.map((item, index) => (
-          <ListItem key={index}>
-            <ListItemText primary={item.title} secondary={`by: ${item.author}`} />
-          </ListItem>
-        ))}
-      </List>
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Image</TableCell>
+              <TableCell>Title</TableCell>
+              <TableCell>Author</TableCell>
+              <TableCell>Actions</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {images.map((item, index) => (
+              <TableRow key={index}>
+                <TableCell>
+                  <img src={`data:image/png;base64,${item.img}`} alt={item.title} width="250" height="150" />
+                </TableCell>
+                <TableCell>{item.title}</TableCell>
+                <TableCell>{item.author}</TableCell>
+                <TableCell>
+                  <IconButton onClick={() => handleDelete(index)}>
+                    <DeleteIcon />
+                  </IconButton>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   );
 }
